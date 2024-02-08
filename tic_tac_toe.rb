f83732c56c8b05885e8b6f5f56987313f46bb75d
Mike Costa <mcosta1@gmail.com>
2024-02-08 18:11:30 -0500

    Implement the Player class
    The class just holds the name, game_mark (which will end up being either
    "X" or "O"), along with a method for a user to enter the next move for
    the Tic Tac Toe game.

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -9,6 +9,22 @@ end
 # This class represents a player, whom will
 # play the Tic Tac Toe game.
 class Player
+  attr_accessor :name
+  attr_reader :game_mark
+
+  def initialize(name, game_mark)
+    @name = name
+    @game_mark = game_mark
+  end
+
+  # Prompts the player to enter a selection,
+  # which is then returned.
+  #
+  # @return [String] Input entered by the player
+  def enter_choice
+    puts "#{name} enter your next move:"
+    gets.chomp
+  end
 end
 
 ##
