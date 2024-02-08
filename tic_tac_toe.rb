5abf74f7b052b946c3f0b138fb4f28bc3fc91acc
Mike Costa <mcosta1@gmail.com>
2024-02-08 17:56:04 -0500

    Add tic_tac_toe.rb file
    Empty classes have been defined for:
    
      TicTacToe
      Player
      GameManager
    
    These classes will be implemented later.

---
+++ tic_tac_toe.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+##
+# This class represents a Tic Tac Toe game.
+class TicTacToe
+end
+
+##
+# This class represents a player, whom will
+# play the Tic Tac Toe game.
+class Player
+end
+
+##
+# This class runs the actual Tic Tac Toe
+# game.
+class GameManager
+end
