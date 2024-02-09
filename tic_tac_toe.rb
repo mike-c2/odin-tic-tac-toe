f1c21d5b5862fca7692b100a7c78decce7a6f001
Mike Costa <mcosta1@gmail.com>
2024-02-08 20:44:59 -0500

    Implement most of class TicTacToe
    The class can create a grid, print out the grid, and enter game choices
    into the grid.
    
    Still remaining to be implemented are the methods for checking whether
    someone has won the game.

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -3,6 +3,91 @@
 ##
 # This class represents a Tic Tac Toe game.
 class TicTacToe
+  GRID_SIZE = 3
+
+  def initialize
+    @columns = []
+    last_column = ('A'.ord + GRID_SIZE - 1).chr
+
+    ('A'..last_column).each do |column|
+      @columns.push(column)
+    end
+
+    @rows = []
+
+    (1..GRID_SIZE).each do |row|
+      @rows.push(row.to_s)
+    end
+
+    new_game
+  end
+
+  def new_game
+    reset_grid
+    reset_valid_choices
+  end
+
+  def play?(game_mark, choice)
+    choice = choice.upcase
+    return false unless @valid_choices.include?(choice)
+
+    @valid_choices.delete(choice)
+
+    column = @columns.find_index(choice[0])
+    row = @rows.find_index(choice[1])
+
+    @grid[row][column] = game_mark
+
+    true
+  end
+
+  def more_choices_remaining?
+    !@valid_choices.empty?
+  end
+
+  def print_game
+    print_columns
+    print_grid
+  end
+
+  private
+
+  def reset_grid
+    @grid = Array.new(GRID_SIZE) { Array.new(GRID_SIZE, ' ') }
+  end
+
+  def reset_valid_choices
+    @valid_choices = []
+
+    @columns.each do |column|
+      @rows.each do |row|
+        @valid_choices.push(column + row)
+      end
+    end
+  end
+
+  def print_columns
+    print '   '
+    @columns.each { |column| print "|  #{column}  " }
+    puts '|'
+  end
+
+  def print_grid
+    horizontal_line = '--'
+    (0...GRID_SIZE).each { |_| horizontal_line += '-------' }
+
+    puts horizontal_line
+
+    (0...GRID_SIZE).each do |row|
+      print " #{@rows[row]} |"
+
+      (0...GRID_SIZE).each do |column|
+        print "  #{@grid[row][column]}  |"
+      end
+
+      puts "\n#{horizontal_line}"
+    end
+  end
 end
 
 ##
@@ -31,4 +116,13 @@ end
 # This class runs the actual Tic Tac Toe
 # game.
 class GameManager
+end
+
+mike = TicTacToe.new
+
+10.times do
+  puts 'Enter something'
+  choice = gets.chomp
+  puts mike.play?('X', choice)
+  mike.print_game
 end
