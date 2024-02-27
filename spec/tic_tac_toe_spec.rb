3f128949e829435d7ebd74d3e4b3d14679e6a648
Mike Costa <mcosta1@gmail.com>
2024-02-27 17:56:11 -0500

    Add unit tests for TicTacToe#played?

--- spec/tic_tac_toe_spec.rb
+++ spec/tic_tac_toe_spec.rb
@@ -1,3 +1,34 @@
 # frozen_string_literal: true
 
 require_relative '../tic_tac_toe'
+
+describe TicTacToe do
+  subject(:game) { described_class.new }
+  let(:game_mark) { 'X' }
+
+  describe '#played?' do
+    context 'when given a valid input as argument' do
+      before do
+        valid_choices = %w[A1 A2 A3 B1 B2 B3]
+        game.instance_variable_set(:@valid_choices, valid_choices)
+      end
+
+      it 'returns true' do
+        choice = 'A1'
+        expect(game).to be_played(game_mark, choice)
+      end
+    end
+
+    context 'when given an invalid input as argument' do
+      before do
+        valid_choices = %w[A2 A3 B1 B2 B3]
+        game.instance_variable_set(:@valid_choices, valid_choices)
+      end
+
+      it 'returns false' do
+        choice = 'A1'
+        expect(game).not_to be_played(game_mark, choice)
+      end
+    end
+  end
+end
